1f1699372b3619e7be0a97dfae41c1853d347a79
Ekaterina Shmeleva <gutkati@mail.ru>
2023-03-16 17:53:44 +0400

    Creat popup

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let popupBg = document.querySelector('.popup__bg')
+let popup = document.querySelector('.popup')
+let openPopupButton = document.querySelectorAll('.open__popup')
+let closePopupButton = document.querySelector('.popup__close')
+
+openPopupButton.forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault()
+        popupBg.classList.add('active')
+        popup.classList.add('active')
+    })
+})
+
+closePopupButton.addEventListener('click', () => {
+    popupBg.classList.remove('active')
+    popup.classList.remove('active')
+})
